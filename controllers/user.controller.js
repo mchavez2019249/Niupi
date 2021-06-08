9b77ce8f5e811670118c693cfa427f37957e3d08
Marlon Chávez <mchavez-2019249@kinal.edu.gt>
2021-06-07 22:18:12 -0600

    deleteUser

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,7 +13,44 @@ var jwt = require('../services/jwt');
 //UPDATE
 
 //DELETE 
+function deleteUser(req, res){
+    let userId = req.params.idU;
+    let params = req.body;
+    if(userId !=req.user.sub){
+        return res.status(403).send({message: 'No tienes permisos para realizar esta acción'})
+    }else{
+        User.findById(userId, (err, userFind)=>{
+            if(err){
+                res.status(500).send({message: 'Error general'})
+            }else if(userFind){
+                if(params.password){
+                    bcrypt.compare(params.password, userFind.password, (err, passwordCheck)=>{
+                        if(err){
+                            res.status(500).send({message: 'Error general', err})
+                        }else if(passwordCheck){
+                                User.findByIdAndRemove(userId, (err, userFind)=>{
+                                    if(err){
+                                        res.status(500).send({message: 'Error general'})
+                                    }else if(userFind){
+                                        res.status(200).send({message: 'Eliminado correctamente'})
+                                    }else{
+                                        res.status(403).send({message: 'NO eliminado'})
+                                    }
+                                })
+                        }else{
+                            res.status(403).send({message: 'Contraseña incorrecta'})
+                        }
+                    })
+                }else{
+                    res.status(200).send({message:'Ingrese la contraseña del usuario para eliminar'})
+                }
+            }else{
+                res.status(403).send({message: 'Usuario inexistente'})
+            }
 
+        })
+    }
+}
 //SEARCH
 
 //LIST
