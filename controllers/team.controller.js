5e566192df550f652a1f62af3224df008cebe692
Marlon Chávez <mchavez-2019249@kinal.edu.gt>
2021-06-09 21:37:00 -0600

    Plantilla CRUD league team

---
+++ controllers/team.controller.js
@@ -0,0 +1,18 @@
+'use strict'
+var Team = require('../models/team.model');
+var jwt = require('../services/jwt');
+
+//SAVE TEAM 
+
+//UPDATE TEAM
+
+//DELETE TEAM
+
+//LIST TEAM
+
+//SEARCH TEAM 
+
+//FUNCTIONS ROUTES
+module.exports = {
+
+}
