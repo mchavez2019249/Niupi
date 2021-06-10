c39e42a091f02998cdeb720c756930d385c15690
Erick Barrera <ebarrera-2019263@kinal.edu.gt>
2021-06-09 22:36:09 -0600

    update league

--- controllers/league.controller.js
+++ controllers/league.controller.js
@@ -5,43 +5,48 @@ var jwt = require('../services/jwt');
 //SAVE LEAGUE
 
 //UPDATE LEAGUE
+function updateLeague(req, res){
+    let userId = req.params.idU;
+    let LeagueId = req.params.idL;
+    let update = req.body;
 
-//DELETE LEAGUE
-function deleteLeague(req, res){
-    var idLeague = req.params.idL;
-    var adminId = req.params.idU;
-    if(adminId !=req.user.sub){
-        res.status(403).send({message: 'No puede acceder a esta funcion'})
+    if(userId != req.user.sub){
+        return res.status(404).send({message: 'No tienes permiso para realizar esta acción'});
     }else{
-        League.findById(idLeague, (err, leagueFind)=>{
-            if(err){
-                res.status(500).send({message: 'ERROR GENERAL'});
-            }else if(leagueFind){
-                if(leagueFind.admin == adminId){
-                    League.findByIdAndRemove(idLeague, (err, leagueDelete)=>{
+        if(update.name && update.phone){
+            User.findOne({_id: userId, Leagues: LeagueId}, (err, userContact)=>{
+                if(err){
+                    return res.status(500).send({message: 'Error general'});
+                }else if(userContact){
+                    League.findByIdAndUpdate(LeagueId, update, {new: true}, (err, updateContact)=>{
                         if(err){
-                            res.status(500).send({message: 'ERROR GENERAL'});
-                        }else if(leagueDelete){
-                            res.send({message: 'Liga eliminada exitosamente'});
+                            return res.status(500).send({message: 'Error general al actualizar'});
+                        }else if(updateContact){
+                            return res.send({message: 'Liga  actualizada', updateContact});
                         }else{
-                            res.send({message: 'Error al eliminar liga'});
+                            return res.status(401).send({message: 'No se pudo actualizar el contacto'});
                         }
-                    });
+                    })
                 }else{
-                    res.status(403).send({message: 'Administrador no autorizado'});
-                }             
-            }else{
-                res.status(403).send({message: 'Administrador no autorizado'});
-            }
-        })
-       
+                    return res.status(404).send({message: 'Usuario o contacto inexistente'});
+                }
+            }) 
+        }else{
+            return res.status(404).send({message: 'Por favor ingresa los datos mínimos'});
+        }       
     }
 }
+//DELETE LEAGUE
+
 //LIST LEAGUE
 
 //SEARCH LEAGUE 
 
 //FUNCTIONS ROUTES
 module.exports = {
-    deleteLeague
+
+
+
+
+updateLeague,
 }
