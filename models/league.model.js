1e32f096c342f4519b88d9c73a1cda4dee683fcf
Oscar Alfaro <oalfaro-2019377@kinal.edu.gt>
2021-06-09 22:22:29 -0600

    Cambio model league

--- models/league.model.js
+++ models/league.model.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var leagueSchema = Schema({
     name: String,
+    admin: {type: Schema.ObjectId, ref: 'user'},
     teams: [{type: Schema.ObjectId, ref: 'team'}]
 })
 module.exports = mongoose.model('league', leagueSchema);
