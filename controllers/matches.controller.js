226b139f3b26f379bda000f8aa2fdc05d3b96152
Marlon Chávez <mchavez-2019249@kinal.edu.gt>
2021-06-14 16:28:10 -0600

    Esquema Matches controller

---
+++ controllers/matches.controller.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var League = require('../models/league.model');
+var Team = require('../models/team.model');
+var Matches = require ('../models/matches.model');
+var jwt = require('../services/jwt');
+
+//SAVE_MATCH
+function saveMatch(req,res){
+
+}
+
+//DELETE_MATCH
+function deleteMatch(req,res){
+    
+
+}
+//LIST_MATCH listar a de mayor a menor
+function listMatches(req,res){
+    
+}
+
+//FUNCTIONS ROUTES
+module.exports = {
+    
+}
