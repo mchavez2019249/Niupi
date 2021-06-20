fef6ede65bc8081d0e591241a057f5df1bba1209
Marlon Chávez <mchavez-2019249@kinal.edu.gt>
2021-06-19 23:34:19 -0600

    Error DeleteMatch

--- controllers/match.controller.js
+++ controllers/match.controller.js
@@ -253,40 +253,6 @@ function saveMatch(req,res){
 
 }
 
-<<<<<<< HEAD
-=======
-//DELETE_MATCH
-function deleteMatch(req,res){
-
-
-}
-
-function deleteMatch(req, res) {
-    let matchId = req.params.id;
-
-    Product.findByIdAndRemove(matchId, (err, matchDeleted) => {
-        if (err) {
-            res.status(500).send({
-                message: 'Error general'
-            });
-        } else {
-            if (matchDeleted) {
-                res.status(200).send({
-                    message: 'Eliminado correctamente',
-                    matchDeleted
-                });
-            } else {
-                res.status(404).send({
-                    message: 'No se elimino'
-                });
-            }
-        }
-    });
-};
-
-
-
->>>>>>> master
 //LIST_MATCH listar a de mayor a menor
 function listMatches(req,res){
     Team.sort({compareFunction}).exec((err, team)=>{
@@ -304,5 +270,5 @@ function listMatches(req,res){
 module.exports = {
     saveMatch,
     listMatches,
-    deleteMatch
+    
 }
